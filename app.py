08ccb9eeb5953729760f1bae7399f47d491fc833
baoc75 <chibao123hcm@gmail.com>
2023-07-05 15:48:54 +0700

    feat: update cm1

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        hoten = request.form['hoten']
+        truong = request.form['truong']
+        return render_template('results.html', hoten=hoten, truong=truong)
+
+
+@app.route('/abc')
+def xinchao():
     btc = True
     danhsach = ["Khoi", "Bao", "Hieu", "Huy"]
     return render_template('index.html', name='Web Workshop', btc=btc, danhsach=danhsach)
